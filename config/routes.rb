f0ca5adecce6cb4b838776264ffd879ddea30fdd
Hidetoshi tsutsumi <tsutsutsutsumi223@gmail.com>
2024-09-04 15:18:39 +0000

    [add]deviseを導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  root 'homes#top'
+  get 'homes/about', as: "about"
+  
+  devise_for :customers, controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+ devise_for :admin, controllers: {
+  sessions: "admin/sessions"
+}
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
