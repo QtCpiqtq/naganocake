8221fd1ed8cfbad98ff65a5fbfad8d363e724080
Hidetoshi tsutsumi <tsutsutsutsumi223@gmail.com>
2024-09-26 14:53:29 +0000

    [fix]注文処理コードのレファクタリング

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,49 +1,54 @@
 class Public::OrdersController < ApplicationController
   def new
-    @orders = Order.new
+    @order = current_customer.orders.build
   end
 
   def confirm
+    @order = current_customer.orders.build(order_params)
     @cart_items = current_customer.cart_items
-    @shipping_fee = 800
-    @total = 0
-    @cart_items.each do |cart_item|
-      @total += cart_item.subtotal
+    @order.shipping_fee = 800
+    #@total = 0
+    @order.price = @cart_items.map { |cart_item| cart_item.subtotal }.sum
+    #@payment_method = order_params[:payment_method]
+
+    #case order_params[:select_address].to_i
+    #when 0
+    #  @order.postal_code = current_customer.postal_code
+    #  @order.address = current_customer.address
+    #  @order.name = current_customer.name
+    #when 1
+    #  address = Address.find(order_params[:address_id])
+    #  @order.postal_code = address.postal_code
+    #  @order.address = address.address
+    #  @order.name = address.name
+    #when 2
+      #@postal_code = order_params[:postal_code]
+      #@address = order_params[:address]
+      #@name = order_params[:name]
+    #end
+    #@order = Order.new
+
+    if !@order.valid?
+      flash.now[:alert] = "failed"
+      render :new
     end
-    @payment_method = order_params[:payment_method]
-    case order_params[:select_address].to_i
-    when 0
-      @postal_code = current_customer.postal_code
-      @address = current_customer.address
-      @name = current_customer.name
-    when 1
-      address = Address.find(order_params[:address_id])
-      @postal_code = address.postal_code
-      @address = address.address
-      @name = address.name
-    when 2
-      @postal_code = order_params[:postal_code]
-      @address = order_params[:address]
-      @name = order_params[:name]
-    end
-    @orders = Order.new
   end
 
   def create
-    order = Order.new(order_params)
-    order.customer_id = current_customer.id
+    order = current_customer.orders.build(order_params)
     order.date = Time.current
     if order.save
-      cart_items = current_customer.cart_items
-      cart_items.each do |cart_item|
-        order_detail  = OrderDetail.new
-        order_detail.order_id = order.id
-        order_detail.item_id = cart_item.item_id
-        order_detail.amount  = cart_item.amount
-        order_detail.price_inc_tax = cart_item.item.with_tax_price
-        order_detail.save
-        cart_item.destroy
-      end
+      flash[:notice] = "注文しました"
+      #cart_items = current_customer.cart_items
+      #cart_items.each do |cart_item|
+      #  order_detail  = OrderDetail.new
+      #  order_detail.order_id = order.id
+      #  order_detail.item_id = cart_item.item_id
+      #  order_detail.amount  = cart_item.amount
+      #  order_detail.price_inc_tax = cart_item.item.with_tax_price
+      #  order_detail.save
+      #  cart_item.destroy
+      #end
     else
       flash[:alert] = "ご注文の確定に失敗しました。"
     end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,42 @@
 class Order < ApplicationRecord
+  attr_accessor :select_address, :address_id
+  
   belongs_to :customer
   has_many :order_details, dependent: :destroy
   
+  before_validation :update_params
+  after_create :create_details
+  
   enum payment_method: { "クレジットカード": 0, "銀行振込": 1 }
   enum status: { "入金待ち": 0, "入金確認": 1, "製作中": 2, "発送準備中": 3, "発送済み": 4 }
+
+  private
+  
+  def update_params
+    case self.select_address.to_i
+    when 0
+      self.postal_code = self.customer.postal_code
+      self.address = self.customer.address
+      self.name = self.customer.name
+    when 1
+      address = Address.find(self.address_id)
+      self.postal_code = address.postal_code
+      self.address = address.address
+      self.name = address.name
+    end
+    self
+  end
+  
+  def create_details
+    cart_items = self.customer.cart_items
+    cart_items.each do |cart_item|
+      order_detail = OrderDetail.new
+      order_detail.order_id = self.id
+      order_detail.item_id = cart_item.item_id
+      order_detail.amount  = cart_item.amount
+      order_detail.price_inc_tax = cart_item.item.with_tax_price
+      order_detail.save!
+    end
+    cart_items.destroy_all
+  end
 end
